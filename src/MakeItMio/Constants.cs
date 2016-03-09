c3b59a00cc5056ae9358b2982b065967419d0f45
Melvin <melvin.rodriguez@prodigious.cr>
2016-03-08 20:37:45 -0600

    Adding gulp and bootstrap

---
+++ src/MakeItMio/Constants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MakeItMio
+{
+    public struct Constants
+    {
+        public struct LayoutParameters
+        {
+            public static string IsFluid => "ContainerIsFluid";
+        }
+    }
+}
